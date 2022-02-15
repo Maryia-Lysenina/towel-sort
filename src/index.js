f09863baf95c4205a6d293e1f35265cba9210f77
Maryia-Lysenina <lysenina.maria@gmail.com>
2022-02-15 21:35:14 +0300

    init: mission is complited

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (arr) {
+
+      let newArr = [];
+      if( typeof arr == 'undefined'){
+          return newArr;
+      } else{
+          fn();
+      }
+      function fn(){
+          for(let i = 0; i < arr.length; i++){
+              if(i % 2 != 0){
+                  arr[i].reverse();
+              }
+          }
+      }
+      function newArrReturn(){
+          for(let i of arr){
+              for(let j of i){
+                  newArr.push(j)
+              }
+          }
+      }
+      newArrReturn();
+      return newArr;
+
 }
